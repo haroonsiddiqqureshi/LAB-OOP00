7927ccadb2119c9e9102b6a588618dfcf26e3607
ffusefs <>
2022-11-28 15:59:14 +0700

    add name id email

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("ฮารูณ ซิดดิ๊ก คูเรซิ");
+Console.WriteLine("653450300-1");
+Console.WriteLine("haroonsiddiq.q@kkumail.com");
